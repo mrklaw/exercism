14abb0a9393c8e5e6f3d20a0f01a08515ccaa5cb
Sloan Poe <sloanpoe@gmail.com>
2014-09-25 13:33:57 -0400

    second revision.  Using translate instead of walking through the string

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,9 +1,7 @@
+import string
+
 def to_rna(dna):
 
-	rna_translate = { 'G':'C','C':'G','T':'A','A':'U' }
-	
-	dna = dna.upper()
-	rna = ""
-	for char in dna:
-		rna += rna_translate[char]
-	return rna
+	rna_translate = string.maketrans('GCTA','CGAU')
+	dna = dna.upper() #just in case
+	return dna.translate(rna_translate)
